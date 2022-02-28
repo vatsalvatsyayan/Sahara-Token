cc14fafaa378476b6cf0b75600e6dcd6210f53e1
Vatsal Vatsyayan <vatsaljackspicer@gmail.com>
2022-02-28 16:05:21 +0300

    1. Smoke Test

---
+++ contracts/DappToken.sol
@@ -0,0 +1,14 @@
+pragma solidity >=0.4.2 <0.9.0;
+
+contract DappToken {
+	// Constructor 
+	// Set the total number of tokens
+	// Read the total number of tokens
+
+	uint256 public totalSupply;
+
+	constructor() public {
+		totalSupply = 1000000;
+
+	}
+}
