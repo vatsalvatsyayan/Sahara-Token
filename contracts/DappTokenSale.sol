bccf8260aff891f4f8d6cf454960a3189a775be4
Vatsal Vatsyayan <vatsaljackspicer@gmail.com>
2022-03-11 23:48:17 +0300

    Token Sale Contd

--- contracts/DappTokenSale.sol
+++ contracts/DappTokenSale.sol
@@ -7,6 +7,9 @@ contract DappTokenSale {
 	address admin;
 	DappToken public tokenContract;
 	uint256 public tokenPrice;
+	uint256 public tokensSold;
+
+	event Sell(address _buyer, uint256 _amount);
 
 	constructor(DappToken _tokenContract, uint256 _tokenPrice) public {
 		//Assign an admin 
@@ -18,4 +21,29 @@ contract DappTokenSale {
 		//Token Price
 		tokenPrice = _tokenPrice;
 	} 
+
+	// multiply 
+	function multiply(uint x, uint y) internal pure returns (uint z) {
+		require(y == 0 || (z = x * y)/ y == x);
+	}
+
+	// Buy Tokens
+	function buyTokens(uint256 _numberOfTokens) public payable {
+
+		//Require that value is equal to tokens
+		require(msg.value == multiply(_numberOfTokens,tokenPrice));
+		
+		// Require that there are enough tokens in the contact
+		require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+
+		//Require that a transfer is successful 
+		require(tokenContract.transfer(msg.sender, _numberOfTokens));
+
+		//keep track of number of tokens sold
+		tokensSold += _numberOfTokens;
+
+		// emit sell event
+		emit Sell(msg.sender, _numberOfTokens);
+
+	}
 }
--- test/DappTokenSale.js
+++ test/DappTokenSale.js
@@ -1,10 +1,16 @@
+const DappToken = artifacts.require("DappToken");
+
 const DappTokenSale = artifacts.require("DappTokenSale");
 
 
-
 contract('DappTokenSale', function(accounts){
+	var tokenInstance;
 	var tokenSaleInstance;
+	var admin = accounts[0];
+	var buyer = accounts[1];
 	var tokenPrice = 1000000000000000; //in wei, smallest unit of ether crypto, used in order to avoid using floats
+	var tokensAvailable = 750000;
+	var numberOfTokens;
 
 	it('initializes the contract with the correct values', function(){
 		return DappTokenSale.deployed().then(function(instance){
@@ -18,6 +24,43 @@ contract('DappTokenSale', function(accounts){
 			return tokenSaleInstance.tokenPrice();
 		}).then(function(price){
 			assert.equal(price, tokenPrice, 'token price is correct');
+		});
+	});
+
+	it('facilitates token buying', function() {
+		return DappToken.deployed().then(function(instance){
+			// Grab token instance first
+			tokenInstance = instance;
+			return DappTokenSale.deployed();
+		}).then(function(instance){
+			// then grab token sale instance
+			tokenSaleInstance = instance;
+			//  Provision 75% of all tokens to the token sale smart contract
+			return tokenInstance.transfer(tokenSaleInstance.address, tokensAvailable, { from: admin })
+		}).then(function(receipt){
+			numberOfTokens = 10;
+			return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: numberOfTokens * tokenPrice})
+		}).then(function(receipt){
+			assert.equal(receipt.logs.length, 1, 'triggers one event');
+			assert.equal(receipt.logs[0].event, 'Sell', 'Should be the "Sell" event');
+			assert.equal(receipt.logs[0].args._buyer, buyer, 'logs the account that purchased the tokens');
+			assert.equal(receipt.logs[0].args._amount, numberOfTokens, 'logs the number of tokens purchased');
+			return tokenSaleInstance.tokensSold();
+		}).then(function(amount){
+			assert.equal(amount.toNumber(), numberOfTokens, 'increments the number of tokens sold');
+			return tokenInstance.balanceOf(buyer);
+		}).then(function(balance){
+			assert.equal(balance.toNumber(),numberOfTokens);
+			return tokenInstance.balanceOf(tokenSaleInstance.address);
+		}).then(function(balance){
+			assert.equal(balance.toNumber(), tokensAvailable - numberOfTokens);
+			//Try to buy tokens different from the ether value
+			return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: 1});
+		}).then(assert.fail).catch(function(error){
+			assert(error.message.indexOf('revert') >= 0, 'msg.value must equal number of tokens in wei');
+			return tokenSaleInstance.buyTokens(800000, { from: buyer, value: numberOfTokens * tokenPrice});
+		}).then(assert.fail).catch(function(error){
+			assert(error.message.indexOf('revert') >= 0, 'Cannot purchase more tokens than available');
 		});
 	});
 });
